5feb2c79cd1804b04f7f483355760bc17b6394fe
dingdong-vamshi <vamshikrishnapendyala19@gmail.com>
2025-06-19 10:29:22 +0530

    fix: resolved merge conflict in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import Login from './pages/Login';
-import './index.css';
-
+import './index.css'; // Tailwind CSS + global styles
 import Navbar from './components/Navbar';
 import ThemeToggle from './components/ThemeToggle';
+import Login from './pages/Login'; //login page
 
 function App() {
   return (
