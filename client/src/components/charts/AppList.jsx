0be21d964e6e462c2959a7f56c3cb8efdf7746a7
Sanjana <sanju250506@gmail.com>
2025-06-25 21:04:18 +0530

    Add Dashboard UI for issue #13

---
+++ client/src/components/charts/AppList.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { FaChrome, FaSafari, FaEdge, FaFigma, FaMicrosoft } from "react-icons/fa";
+import { SiAdobeillustrator, SiMessenger } from "react-icons/si";
+
+const apps = [
+  { name: "YouTube", time: "3 h 10 min", icon: <FaChrome /> },
+  { name: "Instagram", time: "2 h 45 min", icon: <FaEdge /> },
+  { name: "Netflix", time: "2 h 20 min", icon: <FaSafari /> },
+  { name: "WhatsApp", time: "1 h 15 min", icon: <SiMessenger /> },
+  { name: "Facebook", time: "58 min", icon: <FaMicrosoft /> },
+  { name: "Twitter", time: "42 min", icon: <FaFigma /> },
+  { name: "Snapchat", time: "25 min", icon: <SiAdobeillustrator /> },
+];
+
+const AppList = () => (
+  <ul className="space-y-2 text-sm text-gray-700 dark:text-gray-200">
+    {apps.map((app, idx) => (
+      <li key={idx} className="flex justify-between items-center border-b pb-1 border-gray-300 dark:border-gray-600">
+        <div className="flex items-center space-x-2">
+          <span>{app.icon}</span>
+          <span>{app.name}</span>
+        </div>
+        <span>{app.time}</span>
+      </li>
+    ))}
+  </ul>
+);
+
+export default AppList;
